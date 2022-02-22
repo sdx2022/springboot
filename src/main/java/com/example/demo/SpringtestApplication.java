d13752605f97171781fab90fccb693cd4c9068ac
Vannoch Sodex <sdxpm.pro@gmail.com>
2022-02-22 15:19:06 +0700

    code base

---
+++ src/main/java/com/example/demo/SpringtestApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtestApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Java Code goes here");
+		SpringApplication.run(SpringtestApplication.class, args);
+	}
+
+}
